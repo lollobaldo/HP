007dec22acc09ebd287f3c08bae38edbbc01620f
Lorenzo Baldini <lollobaldo2000@gmail.com>
2021-10-31 23:26:07 +0000

    trials

--- codelens-sample/src/extension.ts
+++ codelens-sample/src/extension.ts
@@ -1,32 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, languages, commands, Disposable, workspace, window } from 'vscode';
-import { CodelensProvider } from './CodelensProvider';
+// import { ExtensionContext, commands, window } from 'vscode';
+import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
-let disposables: Disposable[] = [];
-
-export function activate(context: ExtensionContext) {
-    const codelensProvider = new CodelensProvider();
-
-    languages.registerCodeLensProvider("*", codelensProvider);
-
-    commands.registerCommand("codelens-sample.enableCodeLens", () => {
-        workspace.getConfiguration("codelens-sample").update("enableCodeLens", true, true);
-    });
-
-    commands.registerCommand("codelens-sample.disableCodeLens", () => {
-        workspace.getConfiguration("codelens-sample").update("enableCodeLens", false, true);
-    });
-
-    commands.registerCommand("codelens-sample.codelensAction", (args: any) => {
-        console.log(args);
-        window.showInformationMessage(`CodeLens action clicked with args=${args}`);
-    });
-
-    commands.registerCommand('extension.sayHello', async (args, brgs, crgs) => {
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+      vscode.commands.registerCommand('myExtension.sayHello', () => {
         if (!window.activeTextEditor) {
             console.log('quit');
             return;
@@ -37,19 +19,24 @@ export function activate(context: ExtensionContext) {
         inset.onDidDispose(() => {
             console.log('WEBVIEW disposed...');
         });
-        inset.webview.html = `
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAAAUCAYAAAB7wJiVAAAAOUlEQVR42u3RQQ0AAAgEIK9/AcO6aQ0fUIFs1xRvRIgQhAhBiBCECEGIECFCECIEIUIQIgQhQvjiAOZxM5FSZq20AAAAAElFTkSuQmCC">
-        `;
+        inset.webview.html = getWebviewContent();
         console.log('set');
         console.log(inset.webview);
-    });
+      })
+    );
+  }
 
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
-    if (disposables) {
-        disposables.forEach(item => item.dispose());
-    }
-    disposables = [];
-}
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Cat Coding</title>
+  </head>
+  <body>
+      <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+  </body>
+  </html>`;
+  }
+  
